7c9396bdbcd0bf23d87458ed5833c21d8e979af2
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-05-21 16:02:49 +0530

    Pattern

--- Pattern/PrintStarPattern.java
+++ Pattern/PrintStarPattern.java
@@ -1,5 +1,12 @@
 package Pattern;
 
 public class PrintStarPattern {
-    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*"); // Print stars on the same line
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+    }
 }
