1de50558157a925656920d9842bd0e027600f19a
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-05-22 18:30:14 +0530

    CharacterPrint

---
+++ PrintCharacterPattern.java
@@ -0,0 +1,13 @@
+public class PrintCharacterPattern {
+    public static void main(String[] args) {
+       
+        for(int i = 1; i <= 4; i++){
+          char ch = 'A';
+            for(int j = 1; j <= i; j++){
+                System.out.print(ch++);
+            }
+            System.out.println();
+        }
+    }
+    
+}
