511dd66650d8986160b1444407faf5cb502b2f33
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-05-25 19:52:53 +0530

    FindProduct

--- Functions/FindProduct.java
+++ Functions/FindProduct.java
@@ -1,1 +1,19 @@
+package Functions;
 
+import java.util.Scanner;
+
+public class FindProduct {
+
+    public static int multiplyNum(int a, int b) {
+        int product = a * b;
+        return product;
+    }
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        int result = multiplyNum(a, b);
+        System.out.println(result);
+
+    }
+}
