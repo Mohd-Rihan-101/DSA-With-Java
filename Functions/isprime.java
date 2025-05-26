7f359dc8ff999c220136e47f4da31b3c5d699a04
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-05-27 00:45:55 +0530

    isPrime

--- Functions/isprime.java
+++ Functions/isprime.java
@@ -1,17 +1,28 @@
+
+package Functions;
 public class isprime {
 
     public static boolean isPrime(int n){
-     boolean isPrime = true;
-        for(int i=2; i<n-1; i++){
-            if(n % i == 0){
-                isPrime = false;
-                break;
-            }
+    //  boolean isPrime = true;
+    //     for(int i=2; i<n-1; i++){
+    //         if(n % i == 0){
+    //             isPrime = false;
+    //             break;
+    //         }
+    //     }
+    //     return isPrime;
+
+    // optimization code 
+
+    for(int i =2; i<=Math.sqrt(n); i++){
+        if(n % 2 == 0){
+            return false;
         }
-        return isPrime;
+    }
+    return true;
     
     }
     public static void main(String[] args) {
-        System.out.println(isPrime(5));
+        System.out.println(isPrime(4));
     }
 }
