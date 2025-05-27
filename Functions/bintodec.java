5103938ab48bd27c3a11bdbac068d8264040e116
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-05-28 02:56:42 +0530

    BinToDec

---
+++ Functions/bintodec.java
@@ -0,0 +1,24 @@
+package Functions;
+
+public class bintodec {
+    public static void binTodec(int binNum){
+        int myNum = binNum;
+        int pow = 0;
+       int decNum = 0;
+
+       while(binNum > 0){
+        int lastDigit = binNum % 10;
+        decNum = decNum + (lastDigit * (int)Math.pow(2, pow));
+
+        pow++;
+        binNum = binNum / 10;
+       }
+
+       System.out.print("decimal of " + myNum + " = " + decNum);
+
+    }
+    public static void main(String[] args) {
+        binTodec(101);
+        
+    }
+}
