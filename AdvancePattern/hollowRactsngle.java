c0ecfd5786c249d7b06d8be85b2da445a066cbc4
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-05-31 00:53:14 +0530

    Hollow-Rectangle

---
+++ AdvancePattern/hollowRactsngle.java
@@ -0,0 +1,27 @@
+package AdvancePattern;
+
+public class hollowRactsngle {
+
+    public static void hollow_rectangle(int totalRow, int totalCol) {
+        // outer loop
+        for (int i = 1; i <= totalRow; i++) {
+
+            // inner loop
+
+            for (int j = 1; j <= totalCol; j++) {
+                if (i == 1 || i == totalRow || j == 1 || j == totalCol) {
+                    System.out.print("*");
+                } else {
+
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        hollow_rectangle(4, 5);
+
+    }
+}
