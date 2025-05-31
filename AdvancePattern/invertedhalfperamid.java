4eb15a7c4d853371a08f3eeb7d69459f0e977b3e
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-05-31 18:46:13 +0530

    Inverted Half Peramid

--- AdvancePattern/invertedhalfperamid.java
+++ AdvancePattern/invertedhalfperamid.java
@@ -3,25 +3,25 @@ package AdvancePattern;
 public class invertedhalfperamid {
 
     public static void halfPeramid(int n) {
-        for (int i = 1; i <= n; i++) {
 
-            // for spaces
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+        // for row
+       for(int i=1; i<=n; i++){
 
-            }
-
-            // for Print starts
-            for (int k = 1; k <= i; k++) {
-                System.out.print("*");
-            }
-
-            System.out.println();
-
+        // for print spaces
+        for(int j=1; j<=n-i; j++){
+            System.out.print(" ");
         }
+        
+         // for print stars
+        for(int k=1; k<=i; k++){
+            System.out.print("*");
+        }
+
+        System.out.println();
+       }
     }
 
     public static void main(String[] args) {
-        halfPeramid(4);
+        halfPeramid(7);
     }
 }
