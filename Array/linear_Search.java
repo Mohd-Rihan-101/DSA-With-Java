63137e816a2c482cf1d98b0cf93f777b364f7f71
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-06-04 16:59:47 +0530

    Linear Search

---
+++ Array/linear_Search.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class linear_Search {
+    public static int linearSearch(int number[], int key){
+        for(int i=0; i<number.length; i++){
+            if(number[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int number[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        int key = 10;
+
+     System.out.print(linearSearch(number, key));
+    
+    }
+}
