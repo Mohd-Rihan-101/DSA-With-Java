9923e8b99a91236f85cf4dd3bace8f57873fe2f7
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-06-04 16:42:32 +0530

    Array basics

--- Array/arrayBasics.java
+++ Array/arrayBasics.java
@@ -1,17 +1,21 @@
 package Array;
+
 // array is a call by reference  with examples
 public class arrayBasics {
 
-    public static void update(int marks[]){
-        for(int i=0; i<marks.length; i++){
-            marks[i] = marks[i] + 1;
+    // yha sirf mene changes kiye hai print nhi kraye hai
+    public static void update(int marks[]) {
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = marks[i] + 2;
         }
     }
-    public static void main(String[] args) {
-        int marks[] = {79, 95, 100};
-        update(marks);
 
-        for(int i=0; i<marks.length; i++){
+    public static void main(String[] args) {
+        int marks[] = { 95, 98, 99 };
+        update(marks);
+     
+        // yha mene print kraya hai apne array ko 
+        for (int i = 0; i < marks.length; i++) {
             System.out.print(marks[i] + " ");
         }
 
