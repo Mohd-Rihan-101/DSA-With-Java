dedd31b734451601834de89acc9dd29bf55dab8a
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-06-12 02:13:27 +0530

    Pairs Number

---
+++ Array/paires.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class paires {
+
+    public static void pairsNum(int numbers[]) {
+        for (int i = 0; i < numbers.length; i++) {
+            int curr = numbers[i]; // 2 4 6 8 10
+
+            for (int j = i + 1; j < numbers.length; j++) {
+                System.out.print("(" + curr + "," + numbers[j] + ") ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { 2, 4, 6, 8, 10 };
+        pairsNum(numbers);
+    }
+}
